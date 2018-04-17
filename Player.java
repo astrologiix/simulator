a8e5112bed92ad63268d0841cf42455ac53aa08a
belle <ladybelle2002@gmail.com>
2018-04-16 20:09:38 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,8 +1,7 @@
-package juniorprog;
-
 public class Player {
 	String name, gender;
 	int prog;
+	String fileName;
 
 	// constructors
 	public Player() {
@@ -10,7 +9,6 @@ public class Player {
 		gender = "Unknown";
 		prog = 0;
 	}
-
 	public Player(String name, String gender, int prog) {
 		this.name = name;
 		this.gender = gender;
@@ -21,7 +19,6 @@ public class Player {
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getName() {
 		return name;
 	}
@@ -30,17 +27,23 @@ public class Player {
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	
 	public String getGender(){
 		return gender;
 	}
-	
+
 	//prog
 	public void setProgress(int prog){
 		this.prog = prog;
 	}
-	
 	public int getProgress(){
 		return prog;
+	}
+
+	//file
+	public void setFile(String fileName){
+		this.fileName = fileName;
+	}
+	public int getFile(){
+		return fileName;
 	}
 }
