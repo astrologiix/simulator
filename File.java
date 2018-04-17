e07d2e60d9e190a693f7f3a66c34c44057e0c990
Parker <muskatooses@gmail.com>
2018-04-17 08:44:35 -0400

    load file capabilities added - not tested

--- File.java
+++ File.java
@@ -1,10 +1,15 @@
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class File {
 
 	//rewrites files with same name
+	//CHANGE: check if file already exists, make sure user wants to overwrite it
+	//saves array into file
 	public static void save(String saveName, String[] data) throws IOException{
 		// have uhh file name to save to in
 		//CHANGE: create file in appdata and use user.home instead
@@ -23,8 +28,18 @@ public class File {
         writer.close();
 	}
 
-	public static void load(String loadName){
+	//load contents of file into array
+	public static String[] load(String loadName) throws IOException{
+		String fileName = System.getProperty("user.dir\\") + loadName + ".txt";
 
+		BufferedReader in = new BufferedReader(new FileReader(fileName));
+		String str=null;
+		ArrayList<String> lines = new ArrayList<String>();
+		while((str = in.readLine()) != null){
+		    lines.add(str);
+		}
+
+		return lines.toArray(new String[lines.size()]);
 	}
 
 	public static void main(String[] args) throws IOException{
